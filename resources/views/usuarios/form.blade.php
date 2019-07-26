2eb90cb956469266481348b80e076af8681783e1
rvndrupal <rodrigodrupal1@gmail.com>
2019-07-26 18:00:20 -0500

    Se separa el primer formulario de casados

--- resources/views/usuarios/form.blade.php
+++ resources/views/usuarios/form.blade.php
@@ -335,57 +335,7 @@
                         </div>{{-- -soltero --}}
 
                          <div id="casado">
-                                <button type="button" name="add" id="addCasado" class="btn btn-sm btn-success">+</button>
-                             <div class="row" style="border: solid 1px black; padding:11px">
-                                    <div class="col-md-4" >
-                                        <div class="input-group flex-nowrap">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" id="addon-wrapping">Nombre</span>
-                                            </div>
-                                            <input type="text" class="form-control" name="nombres_coy" id="nombres_coy" placeholder="Nombres" aria-label="" aria-describedby="addon-wrapping">
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-4" >
-                                            <div class="input-group flex-nowrap">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" id="addon-wrapping">Paterno</span>
-                                                </div>
-                                                <input type="text" class="form-control" name="primero_coy" id="primero_coy" placeholder="Apellido Paterno" aria-label="Apellido Materno" aria-describedby="addon-wrapping">
-                                            </div>
-                                    </div>
-
-                                    <div class="col-md-4" >
-                                            <div class="input-group flex-nowrap">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" id="addon-wrapping">Materno</span>
-                                                </div>
-                                                <input type="text" class="form-control" name="segundo_coy" id="segundo_coy" placeholder="Apellido Materno" aria-label="Apellido Materno" aria-describedby="addon-wrapping">
-                                            </div>
-                                    </div>
-
-                                    <div class="col-md-4" >
-                                            <div class="input-group flex-nowrap">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" id="addon-wrapping">Curp</span>
-                                                </div>
-                                                <input type="text" class="form-control" name="calle" id="curp_coy" placeholder="Curp" aria-label="Curp" aria-describedby="addon-wrapping">
-                                            </div>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                            <div class="input-group flex-nowrap">
-                                                    <div class="input-group-prepend">
-                                                        <span class="input-group-text" id="inputGroupFileAddon01">Curp</span>
-                                                        </div>
-                                                        <div class="custom-file">
-                                                        <input type="file" class="custom-file-input" name="carga_curp_coy" id="inputGroupFileAddon01"  aria-describedby="inputGroupFileAddon01">
-                                                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-                                                    </div>
-                                            </div>
-                                        </div>
-
-                             </div>
+                             @include('usuarios/form/casado')
 
                          </div>{{-- casados --}}
 
